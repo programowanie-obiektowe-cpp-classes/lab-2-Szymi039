83ca2137abf38d158b8deffb7a8ef6bd309ac3fc
Szymon Wilczek <szymon.wilczek03@gmail.com>
2025-11-06 20:20:28 +0100

    zrobione

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,22 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* obiekt;
+
+public:
+    ResourceManager() : obiekt(new Resource())  {}
+    ~ResourceManager() { delete obiekt; }
+    ResourceManager(const ResourceManager& kopia) { obiekt = new Resource(*kopia.obiekt); }
+    ResourceManager& operator=(const ResourceManager& kopia) {  
+        if (this == &kopia)
+        {
+            return *this;
+        };
+        delete obiekt;
+        obiekt = new Resource(*kopia.obiekt);
+        return *this;
+     }
+
+    double get() { return obiekt->get(); }
 };
